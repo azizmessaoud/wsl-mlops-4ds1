4c2f693c80d69e16214f31efa52a51266f7f64fe
Aziz Messaoud <aziz.messaoud@esprit.tn>
2025-11-21 18:03:26 +0100

    Add wsl-mlops-4ds1 folder as regular directory

---
+++ wsl-mlops-4ds1/README.md
@@ -0,0 +1,2 @@
+# data-science-portfolio
+A portfolio of data science projects showcasing machine learning, data analysis, and visualization skills
---
+++ wsl-mlops-4ds1/test_environment.py
@@ -0,0 +1,6 @@
+# test_environment.py
+import numpy as np
+import pandas as pd
+import sklearn
+import mlflow
+print("Environment is set up correctly!")
